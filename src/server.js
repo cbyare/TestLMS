318c90476581d0a6668514666edaa5408d0783c5
Abdirahman <cbyare444@gmail.com>
2023-09-02 15:20:48 +0300

    pug template engine added

--- src/server.js
+++ src/server.js
@@ -1,9 +1,42 @@
 const express =  require('express')
 const bodyParser = require('body-parser')
+const path =  require('path')
 const app = express()
 
+app.set("view engine", "pug");
 app.use(bodyParser.json())
 app.use('/api/books',require('../src/routes/Books/book'))
+
+app.get('/',(req,res)=>
+{
+
+  // api call
+  
+  data =  [
+    {
+      bookname : "Book 1",
+      author: "Abdirahman",
+      datapublished : "20-09-2024"
+    },
+
+
+    {
+      bookname : "Book 2",
+      author: "Abdirahman",
+      datapublished : "20-09-2024"
+    },
+    {
+      bookname : "Book 3",
+      author: "Abdirahman",
+      datapublished : "20-09-2024"
+    }
+  ]
+ 
+
+
+  res.render(path.join(__dirname+'/template/index'),{data})
+
+})
 
 app.listen(3000,() =>{
   console.log('labarary application is listering port 3000')
