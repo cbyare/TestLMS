9f798d3530dfce989f129aa40b93ce41eb82e234
Abdirahman <cbyare444@gmail.com>
2023-09-02 13:53:51 +0300

    server.js change

--- src/server.js
+++ src/server.js
@@ -3,10 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.json())
-
 app.use('/api/books',require('../src/routes/Books/book'))
-
-
 
 app.listen(3000,() =>{
   console.log('labarary application is listering port 3000')
