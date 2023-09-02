586d1b583c510dd16fb4db483dc202e824fc921d
Abdirahman <cbyare444@gmail.com>
2023-09-02 13:46:37 +0300

    server.js updsted

--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/api/books',require('../src/routes/Books/book'))
+app.use('/api/books',require('../src/routes/Books/book'))
+
+
 
 app.listen(3000,() =>{
   console.log('labarary application is listering port 3000')
