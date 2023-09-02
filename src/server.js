2fa4609db0550f9be99e19923b5d013da9a181a4
Mohamed Haadi <mohamedabukar712@gmail.com>
2023-09-02 13:07:35 +0300

    chafes

--- src/server.js
+++ src/server.js
@@ -5,7 +5,15 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.use('/api/books',require('../src/routes/Books/book'))
+app.get('/books',(req,res)=>
+{
+  res.send({
+
+    name : "Introduction to Nodejs",
+    author : "mohamed abuukar ali",
+    date : "30--8-2023"
+  })
+})
 
 app.listen(3000,() =>{
   console.log('labarary application is listering port 3000')
