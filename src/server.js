f8f7fbf85d0bbc4005955d7f5e2e94ce95144596
Abdirahman <cbyare444@gmail.com>
2023-09-02 13:42:02 +0300

    server.js updsted

--- src/server.js
+++ src/server.js
@@ -4,15 +4,7 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/books',(req,res)=>
-{
-  res.send({
-
-    name : "Introduction to Nodejs",
-    author : "mohamed abuukar ali",
-    date : "30--8-2023"
-  })
-})
+app.get('/api/books',require('../src/routes/Books/book'))
 
 app.listen(3000,() =>{
   console.log('labarary application is listering port 3000')
